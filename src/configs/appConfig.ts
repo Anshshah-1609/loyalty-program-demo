1868b7547906deecb09413df7eaadebd4b0bfefe
Ansh Shah <anshshah0961@gmail.com>
2025-04-25 20:18:32 +0530

    fixed api base url

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
   // apiBaseUrl: "http://localhost:4001/api",
-  apiBaseUrl: "https://demovoucherapi.ogstack.com//api",
+  apiBaseUrl: "https://demovoucherapi.ogstack.com/api",
   clientId: "b0ab5f37-edb8-4ebf-bdfa-f11f0ef56d7d",
   clientSecret:
     "D576A4948E368C32E892332414128E50C8D2999F5D05C822288975140995B8F5",
